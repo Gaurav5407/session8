edda4525eaf9c545fbe25c544bbaa074f1a87684
Gaurav Rangapariya <PCA104@server7.com>
2020-01-21 15:47:59 +0530

    Add project files.

---
+++ Session7/Home/Script.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+    $('.trending-carousel').owlCarousel({
+        margin: 10,
+        navText: ['<img class="arrow-left" src="../Images/arrow-toggle.png"/>', '<img class="arrow-right" src="../Images/arrow-toggle.png" />'],
+        //navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+        nav: true,
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            600: {
+                items: 2,
+
+            },
+            1000: {
+                items: 4,
+
+            }
+        }
+    })
+
+    $('.feature-carousel').owlCarousel({
+        margin: 10,
+        //navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+        navText: ['<img class="arrow-left" src="../Images/arrow-toggle.png"/>', '<img class="arrow-right" src="../Images/arrow-toggle.png" />'],
+        nav: true,
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+
+            },
+            600: {
+                items: 3,
+
+            },
+            1000: {
+                items: 6,
+
+            }
+        }
+    })
+    $('.affordable-carousel').owlCarousel({
+        margin: 10,
+        //navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+        navText: ['<img class="arrow-left" src="../Images/arrow-toggle.png"/>', '<img class="arrow-right" src="../Images/arrow-toggle.png" />'],
+        nav: true,
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            600: {
+                items: 3,
+
+            },
+            1000: {
+                items: 5,
+
+            }
+        }
+    })
+    $('.abroad').dropkick();
+
+
+});
---
+++ Session7/Search/search.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $('.column').owlCarousel({
+        margin: 10,
+        navText: ['<img class="arrow-left" src="../Images/left-white-arrow.png"/>', '<img class="arrow-right" src="../Images/right-white-arrow.png" />'],
+        //navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+        nav: true,
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            600: {
+                items: 1,
+
+            },
+            1000: {
+                items: 1,
+
+            }
+        }
+    })
+    $('.activity-select').dropkick();
+});
