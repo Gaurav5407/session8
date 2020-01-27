3282bffcf7221433c27968e97fe65d9c9d2c9137
Gaurav5407 <40224966+Gaurav5407@users.noreply.github.com>
2020-01-27 17:49:14 +0530

    Add files via upload

---
+++ Search/search.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+    $('.column').owlCarousel({
+        margin: 10,
+        navText: ['<img class="arrow-left" src="../Images/left-white-arrow.png"/>', '<img class="arrow-right" src="../Images/right-white-arrow.png" />'],
+        //navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+        nav: true,
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            600: {
+                items: 1,
+
+            },
+            1000: {
+                items: 1,
+
+            }
+        }
+    })
+    $('#activity').dropkick();
+    $('#age').dropkick();
+    $('.dropkick-day').dropkick();
+    $('.dropkick-month').dropkick();
+    if ($(window).width() >= 768 && $(window).width() <= 1023) {
+        $("#filter").html("...");
+
+    }
+
+    var mySlider = new rSlider({
+        target: '#sampleSlider',
+        values: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+        range: true,
+        tooltip: true,
+        scale: true,
+        labels: true,
+        set: [2010, 2013]
+    });
+
+  
+
+});
+function myFunction() {
+    document.getElementById("myForm").reset();
+}
